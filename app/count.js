ba714dd3bc0c6a2f77d1c128ade32fe3ede4a74a
Blake Tidwell <batidwell@gmail.com>
2013-06-12 12:30:02 -0700

    Adds passing behavior for counting specs

--- app/count.js
+++ app/count.js
@@ -3,6 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      console.log(start)
+      var i = start + 1,
+        counter = setInterval(function(){
+          if(i <= end){
+            console.log(i++);
+          }
+        }, 1000);
+
+      return {
+        cancel: function(){
+          clearInterval(counter);
+        }
+      };
     }
   };
 });
